d4dac680e5a1163ffd8b37cf34159cb3ea6c9d37
Sasja <Alexander.Steen.Gent@gmail.com>
2014-10-01 09:20:53 +0200

    added some comments

--- app/src/main/java/com/pollytronics/festivalradar/AbstractSubService.java
+++ app/src/main/java/com/pollytronics/festivalradar/AbstractSubService.java
@@ -6,7 +6,18 @@ import android.os.Looper;
 /**
  * Created by pollywog on 9/20/14.
  * abstract class for helper classes for the RadarService class
- * rs refers to the RadarService instance containing all other helper instances
+ * rs refers to the RadarService instance containing this and all other helper instances,
+ * it provides appropriate interfaces through
+ *
+ * getRadarService()
+ * getRadarDatabase()
+ * getRadarPreferences()
+ *
+ * and it provides a Handler to post tasks on the main thread through
+ *
+ * getMainHandler()
+ *
+ * a few methods need to be overridden to handle some events.
  */
 abstract public class AbstractSubService {
 
@@ -32,10 +43,10 @@ abstract public class AbstractSubService {
     private Handler handler = new Handler(Looper.getMainLooper());
     protected Handler getMainHandler(){
         return handler;
-    };
+    }
 
     /*
-        Override the following 4 methods to handle these events
+        Override the following 5 methods to handle these events
         and add methods to handle the sub classes responsibilities
      */
 

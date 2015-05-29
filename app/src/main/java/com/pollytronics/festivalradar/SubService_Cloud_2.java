5b70790f430841af016af204afa907d6abf9646d
Sasja <Alexander.Steen.Gent@gmail.com>
2015-05-29 14:03:11 +0200

    increased version number, work on SubService_Cloud_2 fixed about text

--- app/src/main/java/com/pollytronics/festivalradar/SubService_Cloud_2.java
+++ app/src/main/java/com/pollytronics/festivalradar/SubService_Cloud_2.java
@@ -6,16 +6,32 @@ import android.net.NetworkInfo;
 import android.os.AsyncTask;
 import android.util.Log;
 
+import com.pollytronics.festivalradar.lib.RadarBlip;
+import com.pollytronics.festivalradar.lib.RadarContact;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 /**
  * Cloud SubService 2
  *
  * periodically pulls and pushes data from server and updates the local database
  *
  * TODO: this supersedes Cloud Subservice 1
- * TODO: use HttpUrlConnection instead of the apache lib, it should improve battery drain i've read somewhere (>=Gingerbread...)
+ * TODO: use HttpUrlConnection instead of the apache lib, it should improve battery drain i've read somewhere (>=Gingerbread (api 9-10)...)
  * TODO: minimize tcp connection lifetime to minimize load on server
  * TODO: also the apache lib is depreciated
  * TODO: further develop the APICall classes for the different calls
+ * http://developer.android.com/training/basics/network-ops/connecting.html
  *
  * Created by pollywog on 26/5/2015.
  */
@@ -72,7 +88,7 @@ public class SubService_Cloud_2 extends SubService {
 
     @Override
     protected void onUnregister() {
-        Log.i(TAG,"onUnregister");
+        Log.i(TAG, "onUnregister");
     }
 
     @Override
@@ -83,41 +99,191 @@ public class SubService_Cloud_2 extends SubService {
         getMainHandler().post(cloudLoop);
     }
 
-    private class SyncToWebserviceTask extends AsyncTask<Void, Void, String> {  // TODO: this class does the actual api calls using the APICall classes
+    private String myHttpGet(String myurl) throws IOException {   // TODO: study this code
+        InputStream is = null;
+        URL url = new URL(myurl);
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        try {
+            conn.setReadTimeout(1000);
+            conn.setConnectTimeout(10000);
+            conn.setRequestMethod("GET");
+            conn.setDoInput(true);
+            Log.i(TAG, "GET " + myurl);
+            conn.connect();
+            int response = conn.getResponseCode();
+            Log.i(TAG, "HTTP RESPONSE CODE: " + response);
+            if(response!=200) throw new IOException();
+            is = conn.getInputStream();
+            String contentString = readInputStream(is);
+            return contentString;
+        } finally {
+            if (is != null) is.close();
+            conn.disconnect();
+        }
+    }
 
+    private String myHttpPost(String myurl, String data) throws IOException {   // TODO: study this code, the api returns a Error 415 Unsupported media type, this is not how a proper post is done
+        InputStream is = null;
+        OutputStream os = null;
+        URL url = new URL(myurl);
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        try {
+            conn.setReadTimeout(1000);
+            conn.setConnectTimeout(10000);
+            conn.setRequestMethod("POST");
+            conn.setDoInput(true);
+            conn.setDoOutput(true);
+            Log.i(TAG, "POST " + myurl);
+            Log.i(TAG, "BODY = " + data);
+            os = conn.getOutputStream();
+            writeToOutputStream(os, data);
+            conn.connect();
+            int response = conn.getResponseCode();
+            Log.i(TAG, "HTTP RESPONSE CODE: " + response);
+            if(response!=200) throw new IOException();
+            is = conn.getInputStream();
+            String contentString = readInputStream(is);
+            return contentString;
+        } finally {
+            if (is != null) is.close();
+            conn.disconnect();
+        }
+    }
+
+    /**
+     * helper method to convert input stream to string
+     * http://stackoverflow.com/questions/2492076/android-reading-from-an-input-stream-efficiently
+     * TODO: study this code, it might remove all newlines
+     */
+    private String readInputStream(InputStream is) throws IOException {
+        BufferedReader r = new BufferedReader(new InputStreamReader(is));
+        StringBuilder total = new StringBuilder();
+        String line;
+        while ((line = r.readLine()) != null) {
+            total.append(line);
+        }
+        return total.toString();
+    }
+
+    /** helper method to write string into a output stream
+     * TODO: study this code
+     */
+    private void writeToOutputStream(OutputStream os, String data) throws IOException {
+        OutputStreamWriter writer = new OutputStreamWriter(os);
+        writer.write(data, 0, data.length());
+    }
+
+    /**
+     * This class is used to bundle all the interactions that happen with the api into one AsyncTask
+     * it relies on APICall objects to handle the api details of each api separate interaction.
+     * doInBackground can communicate with the other methods by setting member fields, it's safe:
+     * http://developer.android.com/reference/android/os/AsyncTask.html (See Memory observability)
+     */
+    private class SyncToWebserviceTask extends AsyncTask<Void, Void, String> {
+        private APICallSetMyBlip setMyBlip = new APICallSetMyBlip();
+        /**
+         * Gathers all the data needed to perform the api calls
+         */
         @Override
         protected void onPreExecute() {
-            Log.i(TAG, "gathering the data i need to send to webservice");      // TODO: use the api call classes here
+            Log.i(TAG, "gathering the data i need to send to webservice");
+            setMyBlip.collectData();
         }
 
+        /**
+         * does the http requests to the api in background, results should be stored in member fields for onPostExecute to work on.
+         * answers may be parsed here in order to construct and do new calls. Do not call collectData() or handleResults() here,
+         * the other APICall methods should be safe to call from this other thread.
+         */
         @Override
         protected String doInBackground(Void... params) {
-            Log.i(TAG, "doing the httprequest to the webservice");              // TODO: use the api call classes here
-            return "dummy response";
+            Log.i(TAG, "doing the httprequests to the webservice");
+            try {
+                setMyBlip.parseContent(myHttpPost(setMyBlip.getApiQueryString(), setMyBlip.getApiBodyString()));
+            } catch (IOException e) {
+                Log.i(TAG, "IOException: unable to complete all API requests");
+                setMyBlip.flagFailed();
+                return "IOExcepion: unable to complete all API requests";
+            }
+            return null;
         }
 
+        /**
+         * call the APICall methods to process the results
+         */
         @Override
         protected void onPostExecute(String s) {
-            Log.i(TAG, "parsing and using the response of the webservice");     // TODO: use the api call classes here
-            Log.i(TAG, "the response was: " + s);
+            if (setMyBlip.hasFailed()) {
+                Log.i(TAG, "the api call has failed, not calling doTheWork()");
+            } else {
+                Log.i(TAG, "parsing and using the response of the webservice");
+                setMyBlip.doTheWork();
+            }
         }
     }
 
+    /**
+     * APICall object implement REST API Calls, separating the work in:
+     * 1) collecting all the data needed for the call
+     * 2) setting extra parameters manually
+     * 3) constructing the queryString
+     * 4) parsing a response into member fields
+     * 5) using the results
+     *
+     * only 1 and 5 should access data outside the object itself as all others might be called on another thread
+     * TODO: check the error handling of this thing
+     */
     abstract private class APICall {
         protected final String baseUrl = "http://festivalradarservice.herokuapp.com/webservice/";
-        public String getApiCallUrl() { return baseUrl + getApiMethodName(); }
+        private boolean failed = false;
 
-        protected abstract String getApiMethodName();
+        public void flagFailed() { failed = true; }
+        public boolean hasFailed() { return failed; }
+        public abstract void collectData();
+        public abstract String getApiQueryString();
+        public abstract void parseContent(String content);
+        public abstract void doTheWork();
     }
 
-    private class APICallSetMyBlip extends APICall {                            // TODO: these kind of classes should prepare api calls and use the responses
-        @Override
-        protected String getApiMethodName() { return "setMyBlip"; }
+    private class APICallSetMyBlip extends APICall {
+        protected final String apiMethodName = "setMyBlip";
+        String contentString = "";
+        private JSONObject selfBlipJSON = new JSONObject();
 
-        public String getQueryString() {
-            return "dummyQueryString";
+        @Override
+        public void collectData(){
+            Log.i(TAG, "collecting data for APICallSetMyBlip");
+            RadarContact selfContact = getRadarDatabase().getSelfContact();
+            RadarBlip selfBlip = selfContact.getLastBlip();
+            try {
+                selfBlipJSON.put("lat", selfBlip.getLatitude());
+                selfBlipJSON.put("lon", selfBlip.getLongitude());
+                selfBlipJSON.put("time", selfBlip.getTime());
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
         }
 
+        @Override
+        public String getApiQueryString() {
+            return baseUrl+apiMethodName;    // TODO: remove
+        }
 
+        public String getApiBodyString(){
+            return selfBlipJSON.toString();
+        }
+
+        @Override
+        public void parseContent(String content) {
+            Log.i(TAG, "parsing the content retrieved from API");
+            Log.i(TAG, "content retrieved = "+content);
+            contentString = content;
+        }
+
+        @Override
+        public void doTheWork() {
+            Log.i(TAG, "doing the work to process the information from the api call");
+            Log.i(TAG, contentString);
+        }
     }
 }

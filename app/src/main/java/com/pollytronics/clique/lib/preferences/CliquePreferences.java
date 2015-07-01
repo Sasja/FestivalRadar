c7e452dec9765e3bee54a9d78294ca29e9cbaef2
Sasja <Alexander.Steen.Gent@gmail.com>
2015-07-01 16:17:39 +0200

    fixed singleton design pattern bug + code cleanup

--- app/src/main/java/com/pollytronics/clique/lib/preferences/CliquePreferences.java
+++ app/src/main/java/com/pollytronics/clique/lib/preferences/CliquePreferences.java
@@ -6,8 +6,11 @@ import android.preference.PreferenceManager;
 import android.util.Log;
 
 /**
- * Created by pollywog on 9/23/14.
- * TODO: something is very wrong here with the instance always being null, and getInstance always returning a new object...
+ * Class that wraps the sharedPreferences.
+ * It uses the singleton design pattern: http://www.javaworld.com/article/2073352/core-java/simply-singleton.html
+ *
+ * The instance should always be obtained through getInstance(), convenience methods such as getCliquePreferences() are provided for this in a few base classes.
+ * Not threadsafe.
  */
 public class CliquePreferences {
     public static final String COMPASS_ENABLED = "compassEnabled";
@@ -16,7 +19,6 @@ public class CliquePreferences {
     public static final int UPDATE_RATE_LO_BAT = 0;
     public static final int UPDATE_RATE_BALANCED = 1;
     public static final int UPDATE_RATE_HI_PERFORMANCE = 2;
-    @SuppressWarnings("unused")
     static final String TAG = "CliquePreferences";
     private static final String UPDATE_RATE = "updateRate";
     private static final boolean COMPASS_ENABLED_DEFAULT = true;
@@ -33,16 +35,18 @@ public class CliquePreferences {
     }
 
     public static CliquePreferences getInstance(Context context) {
-        if(instance==null) {
-            return new CliquePreferences(context);
-        } else {
-            return instance;
+        if(instance == null) {
+            Log.i(TAG, "creating a new instance of CliquePreferences");
+            instance = new CliquePreferences(context);
         }
+        return instance;
     }
 
+    /**
+     * @return the desired localisation update interval in milliseconds
+     */
     public long getLocalisationUpdateTime_ms(){
-        int setting = preferences.getInt(UPDATE_RATE, UPDATE_RATE_DEFAULT);
-        switch (setting) {
+        switch (getUpdateRate()) {
             case UPDATE_RATE_LO_BAT:
                 return 60 * 1000;
             case UPDATE_RATE_BALANCED:
@@ -54,9 +58,13 @@ public class CliquePreferences {
                 return 15 * 1000;
         }
     }
+
+    /**
+     * @return the desired cloud connection rate in milliseconds
+     */
     public long getCloudUpdateTime_ms(){
         int setting = preferences.getInt(UPDATE_RATE, UPDATE_RATE_DEFAULT);
-        switch (setting) {
+        switch (getUpdateRate()) {
             case UPDATE_RATE_LO_BAT:
                 return 300 * 1000;
             case UPDATE_RATE_BALANCED:
@@ -69,24 +77,27 @@ public class CliquePreferences {
         }
     }
 
+    /**
+     * @return the set value for the update rate, use the constants to decode
+     */
     public int getUpdateRate() {
         return preferences.getInt(UPDATE_RATE, UPDATE_RATE_DEFAULT);
     }
 
+    /**
+     * @param setting use the class constants to set this value
+     */
     public void setUpdateRate(int setting) {
         Log.i(TAG, "setting update rate to " + String.valueOf(setting));
         preferences.edit().putInt(UPDATE_RATE, setting).apply();
     }
 
     public boolean getCompassEnabled() { return preferences.getBoolean(COMPASS_ENABLED, COMPASS_ENABLED_DEFAULT); }
-
     public void setCompassEnabled(Boolean enabled) { preferences.edit().putBoolean(COMPASS_ENABLED, enabled).apply(); }
 
     public boolean getSunEnabled() { return preferences.getBoolean(SUN_ENABLED, SUN_ENABLED_DEFAULT); }
-
     public void setSunEnabled(Boolean enabled) { preferences.edit().putBoolean(SUN_ENABLED, enabled).apply(); }
 
     public double  getZoomRadius() { return preferences.getFloat(ZOOM_RADIUS, ZOOM_RADIUS_DEFAULT); }
-
     public void setZoomRadius(double zoomRadius) { preferences.edit().putFloat(ZOOM_RADIUS, (float) zoomRadius).apply(); }
 }

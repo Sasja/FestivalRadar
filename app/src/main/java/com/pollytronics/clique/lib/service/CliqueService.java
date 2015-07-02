7147ae6335f7f707476f51b699e31bedd215c737
Sasja <Alexander.Steen.Gent@gmail.com>
2015-07-02 14:54:35 +0200

    cleanup, documentation, some renaming variables

--- app/src/main/java/com/pollytronics/clique/lib/service/CliqueService.java
+++ app/src/main/java/com/pollytronics/clique/lib/service/CliqueService.java
@@ -9,20 +9,19 @@ import android.os.IBinder;
 import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 
-import com.pollytronics.clique.R;
 import com.pollytronics.clique.CliqueActivity_Main;
+import com.pollytronics.clique.R;
+import com.pollytronics.clique.SubService_Cloud_2;
+import com.pollytronics.clique.SubService_Localisation;
 import com.pollytronics.clique.lib.CliqueActivity_Interface4CliqueService;
 import com.pollytronics.clique.lib.database.CliqueDb_Interface;
 import com.pollytronics.clique.lib.database.CliqueSQLite.CliqueSQLite;
-import com.pollytronics.clique.SubService_Cloud_2;
-import com.pollytronics.clique.SubService_Localisation;
 
 /**
- * The CliqueService class manages the connection to CliqueActivity and derived classes
- * it creates a few helper classes derived from the SubService class to implement its features and to delegate calls to
- * This class forms a layer between SubService classes and RadarActivities:
- *      - This class delegates calls from activities to the right helper classes (SubServices)
- *      - This class provides methods to the SubServices to reach the Activities.
+ * The CliqueService class provides access to active CliqueActivity instances if any for SubServices. (e.g. to notify them of new data to present)
+ * The actual work is done in the SubService classes that are created and managed by this class.
+ *
+ * TODO: use a list of subServices to pass on the onEvent callbacks instead of manually calling them
  */
 public class CliqueService extends Service implements CliqueService_Interface4SubService, CliqueService_Interface4CliqueActivity {
 
@@ -30,58 +29,60 @@ public class CliqueService extends Service implements CliqueService_Interface4Su
     private final CliqueBinder cliqueBinder = new CliqueBinder();
     private final SubService_Localisation subServiceLocalisation = new SubService_Localisation(this);
     private final SubService_Cloud_2 subServiceCloud = new SubService_Cloud_2(this);
-    private CliqueActivity_Interface4CliqueService ra;
+    private CliqueActivity_Interface4CliqueService registeredCliqueActivity;
     private Boolean raRegistered = false;
 
-    /*
-    create an instance of each helper class here, and add calls in onCreate, onDestroy, onRegister and onUnregister
+    public CliqueService() {}
+
+    /**
+     * Convenience method to retrieve the database instance.
+     * @return the database instance
      */
-
-    public CliqueService() {
-    }
-
-    public CliqueDb_Interface getCliqueDb(){
+    public CliqueDb_Interface getCliqueDb() {
         return CliqueSQLite.getInstance(this);
     }
 
     /**
-     * calls onCreate methods of all subServices
+     * Forwards onCreate() call to all subServices.
      */
     @Override
     public void onCreate() {
-        Log.i(TAG, "onCreate, initialising sub services");
+        Log.i(TAG, "onCreate, initialising subServices");
         subServiceLocalisation.onCreate();
         subServiceCloud.onCreate();
     }
 
     /**
-     * calls onDestroy methods of all subservices
+     * Forwards onDestroy() call to all subServices.
      */
     @Override
     public void onDestroy() {
-        Log.i(TAG, "onDestroy, terminating sub services");
+        Log.i(TAG, "onDestroy, terminating subServices");
         subServiceLocalisation.onDestroy();
         subServiceCloud.onDestroy();
     }
 
     /**
-     * calls onRegister methods of all subservices
+     * Forwards onRegister() call to all subServices.
      */
     private void onRegister() {
-        Log.i(TAG,"onRegister, calling subservice methods");
+        Log.i(TAG,"onRegister, calling subService methods");
         subServiceLocalisation.onRegister();
         subServiceCloud.onRegister();
     }
 
     /**
-     * calls onUnregister methods of all subservices
+     * Forwards onUnregister() call to all subServices.
      */
     private void onUnregister() {
-        Log.i(TAG,"onUnRegister, calling subservice methods");
+        Log.i(TAG,"onUnregister, calling subService methods");
         subServiceLocalisation.onUnregister();
         subServiceCloud.onUnregister();
     }
 
+    /**
+     * Forwards notifyNewSettings() call to all subServices so they can reload them.
+     */
     @Override
     public void notifyNewSettings() {
         subServiceLocalisation.onNewSettings();
@@ -89,8 +90,8 @@ public class CliqueService extends Service implements CliqueService_Interface4Su
     }
 
     /**
-     * gets called each time when an activity calls startService
-     * launches a sticky notification pointing back to CliqueActivity_Main
+     * Gets called each time when an activity calls startService.
+     * Also launches a sticky notification pointing back to the CliqueActivity_Main activity class.
      */
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
@@ -107,7 +108,11 @@ public class CliqueService extends Service implements CliqueService_Interface4Su
     }
 
     /**
-     * gets called when activity wants to bind, it sends the binder back
+     * Gets called when an activity wants to bind. The return value is provided to the binding activity through the onServiceConnected callback.
+     * The CliqueBinder class only provides one method to retrieve the instance of this Service.
+     * It's a bit of a hack around the whole IBinder thing to enable activities and services to call each others methods directly.
+     * (See also class CliqueBinder)
+     *
      * @return throwaway instance of CliqueBinder class
      */
     @Override
@@ -116,49 +121,51 @@ public class CliqueService extends Service implements CliqueService_Interface4Su
     }
 
     /**
-     * gets called from CliqueActivity to pass and save its instance in the CliqueService
-     * will only remember the last calling activity
-     * it will call onRegister methods on SubServices
+     * Gets called from CliqueActivity to pass and save its instance in the CliqueService.
+     * The service will only remember the last calling activity.
+     * It will also call onRegister methods on SubServices.
      */
     public void registerActivity(CliqueActivity_Interface4CliqueService ra){
         Log.i(TAG,"registering activity");
-        this.ra = ra;
+        this.registeredCliqueActivity = ra;
         raRegistered = true;
         onRegister();
     }
 
     /**
-     * gets called from CliqueActivity
-     * forget a certain CliqueActivity instance.
-     * If the one calling doesn't match the current one, do nothing
-     * will call onUnregister methods on SubServices
+     * Gets called from CliqueActivity.
+     * Makes the Service Forget a certain CliqueActivity instance.
+     * If the one calling doesn't match the current one: do nothing.
+     * It will also call onUnregister methods on SubServices
      */
     public void unregisterActivity(CliqueActivity_Interface4CliqueService ra){
-        if (this.ra == ra) {
+        if (this.registeredCliqueActivity == ra) {
             Log.i(TAG,"unregistering activity");
-            this.ra = null;
+            this.registeredCliqueActivity = null;
             raRegistered = false;
             onUnregister();
         }
     }
 
      /**
-     * send text to a registered activity if any
-     * called from within RadarServices and SubServices
+     * Sends text to a registered activity if any.
+     * To be called from within RadarServices itself or SubServices.
      */
     @Override
     public void sendStringToRa(String text){
         if(raRegistered){
-            ra.print(text);
+            registeredCliqueActivity.print(text);
         }
     }
 
-    //------------- Method implementations for SubserviceInterface
-
+    /**
+     * Notifies the registered activity (if any) of database updates.
+     * To be called from within RadarService itself or SubServices.
+     */
     @Override
     public void notifyNewData() {
         if(raRegistered){
-            ra.notifyDatabaseUpdate();
+            registeredCliqueActivity.notifyDatabaseUpdate();
         }
     }
 
@@ -168,14 +175,12 @@ public class CliqueService extends Service implements CliqueService_Interface4Su
     }
 
     /**
-     * throwaway class for activity and services getting each others instances
+     * A throwaway class for activity and services getting each others instances.
+     * A bit of a hack around it's intended use. (See also onBind())
      */
     public class CliqueBinder extends Binder {
         public CliqueService getCliqueService(){
             return CliqueService.this;
         }
     }
-
-    //------------ Method implementations/delegations for RadarActivityInterface
-
 }

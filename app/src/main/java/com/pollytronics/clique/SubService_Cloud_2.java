4af5f6886975ab3a18fa57592f2eb4a2327d6a11
Sasja <Alexander.Steen.Gent@gmail.com>
2015-09-12 10:21:00 +0200

    removed redundant old sync class (api/v1)

--- app/src/main/java/com/pollytronics/clique/SubService_Cloud_2.java
+++ app/src/main/java/com/pollytronics/clique/SubService_Cloud_2.java
@@ -3,21 +3,10 @@ package com.pollytronics.clique;
 import android.content.Context;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
-import android.os.AsyncTask;
 import android.util.Log;
 
-import com.pollytronics.clique.lib.api_v01.ApiCallGetBlips;
-import com.pollytronics.clique.lib.api_v01.ApiCallSetMyBlip;
-import com.pollytronics.clique.lib.base.Blip;
-import com.pollytronics.clique.lib.database.CliqueDbException;
-import com.pollytronics.clique.lib.database.cliqueSQLite.local.DbSelfBlip;
 import com.pollytronics.clique.lib.service.CliqueService;
 import com.pollytronics.clique.lib.service.SubService;
-
-import org.json.JSONException;
-
-import java.io.IOException;
-import java.util.List;
 
 /**
  * Cloud SubService 2
@@ -46,12 +35,7 @@ public class SubService_Cloud_2 extends SubService {
                 NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
                 if (networkInfo != null && networkInfo.isConnected()) {
                     Log.i(TAG, "network available: syncing data");
-
-                    //FIXME testing ---------------------------
-                    //new SyncToWebserviceTask().execute();
                     CliqueSyncer.getInstance(getCliqueService().getContext()).poke();
-                    //FIXME testing ---------------------------
-
                     getMainHandler().removeCallbacks(cloudLoop);    //make sure we dont have 2 loops
                     if(!cleaningUp) getMainHandler().postDelayed(cloudLoop,updateTime_ms);
                 } else {
@@ -100,76 +84,5 @@ public class SubService_Cloud_2 extends SubService {
         updateTime_ms = (int) getCliquePreferences().getCloudUpdateTime_ms();
         Log.i(TAG, "set updateTime to (ms) " + Integer.toString(updateTime_ms));
         getMainHandler().post(cloudLoop);
-    }
-
-    /**
-     * This class is used to bundle all the interactions that happen with the api into one AsyncTask
-     * it relies on APICall objects to handle the api details of each api separate interaction.
-     * doInBackground can communicate with the other methods by setting member fields, it's safe:
-     * http://developer.android.com/reference/android/os/AsyncTask.html (See Memory observability)
-     * TODO: (syncing) might be prettier to return a boolean for success from doInbackground instead of using a var
-     */
-    private class SyncToWebserviceTask extends AsyncTask<Void, Void, String> {
-        private ApiCallSetMyBlip setMyBlip;
-        private ApiCallGetBlips getBlips;
-        private boolean apiCallsSucceeded = false;
-        /**
-         * Gathers all the data needed to perform the api calls
-         */
-        @Override
-        protected void onPreExecute() {
-            Log.i(TAG, "gathering the data i need to send to webservice");
-            long selfId = getCliquePreferences().getAccountId();
-            Blip lastBlip = null;
-            try {
-                lastBlip = DbSelfBlip.getLast();
-            } catch (CliqueDbException e) {
-                e.printStackTrace();
-            }
-            try {
-                setMyBlip = new ApiCallSetMyBlip(lastBlip, selfId);
-                getBlips  = new ApiCallGetBlips(selfId);
-            } catch (JSONException e) {
-                e.printStackTrace();
-            }
-        }
-
-        @Override
-        protected String doInBackground(Void... params) {
-            Log.i(TAG, "calling api from SyncToWebserviceTask");
-            try {
-                setMyBlip.callAndParse();
-                getBlips.callAndParse();
-                apiCallsSucceeded = true;
-            } catch (IOException e) {
-                Log.i(TAG, "IOException: unable to complete all API requests");
-                e.printStackTrace();
-                return "IOExcepion: unable to complete all API requests";
-            } catch (JSONException e) {
-                e.printStackTrace();
-            }
-            return null;
-        }
-
-        /**
-         * call the APICall methods to process the results
-         */
-        @Override
-        protected void onPostExecute(String s) {
-            if (apiCallsSucceeded) {         // only take action if all api calls were successful
-                Log.i(TAG, "using/aplying the responses of the webservice");
-                List<Blip> blips = getBlips.getBlipList();
-                for(Blip b : blips) {
-                    try {
-                        com.pollytronics.clique.lib.database.cliqueSQLite.sync.DbBlip.add(b.getOwnerId(), b);   // TODO: decide if Blips can contain owner id or not
-                    } catch (CliqueDbException e) {
-                        e.printStackTrace();
-                    }
-                }
-                getCliqueService().notifyNewData();
-            } else {
-                Log.i(TAG, "the api call has failed, not calling doTheWork() methods for the calls");
-            }
-        }
     }
 }

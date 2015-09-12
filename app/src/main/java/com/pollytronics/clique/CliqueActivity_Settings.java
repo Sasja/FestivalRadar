c46407a8e086af54ae4e1abc78c847d7616c49e9
Sasja <Alexander.Steen.Gent@gmail.com>
2015-09-12 09:19:17 +0200

    removed change id debugging option, removed sync after profile update

--- app/src/main/java/com/pollytronics/clique/CliqueActivity_Settings.java
+++ app/src/main/java/com/pollytronics/clique/CliqueActivity_Settings.java
@@ -1,8 +1,5 @@
 package com.pollytronics.clique;
 
-import android.content.Context;
-import android.net.ConnectivityManager;
-import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
@@ -14,21 +11,20 @@ import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.Spinner;
-import android.widget.Toast;
 
 import com.pollytronics.clique.lib.CliqueActivity;
 import com.pollytronics.clique.lib.base.Profile;
 import com.pollytronics.clique.lib.database.CliqueDbException;
 import com.pollytronics.clique.lib.database.cliqueSQLite.local.DbSelfProfile;
+import com.pollytronics.clique.lib.tools.MyAssortedTools;
 
 /**
- * TODO: (syncing) syncing local and remote database for self profile should happen more elegantly, now its done at hoc and prone to all kinds of problems
+ * blah
  */
 public class CliqueActivity_Settings extends CliqueActivity implements AdapterView.OnItemSelectedListener{
 
     private static final String TAG = "CliqueActivity_Settings";
 
-    private EditText setIdEditText;
     private EditText setNameEditText;
 
     @Override
@@ -44,8 +40,6 @@ public class CliqueActivity_Settings extends CliqueActivity implements AdapterVi
         updateRateSpinner.setOnItemSelectedListener(this);
         updateRateSpinner.setSelection(getCliquePreferences().getUpdateRate());   // call this at the end or it will not work
 
-        setIdEditText = (EditText) findViewById(R.id.edittext_setid);
-        Button setIdButton = (Button) findViewById(R.id.button_setid);
         Button setNameButton = (Button) findViewById(R.id.button_setname);
         setNameEditText = (EditText) findViewById(R.id.edittext_setname);
 
@@ -54,34 +48,34 @@ public class CliqueActivity_Settings extends CliqueActivity implements AdapterVi
         CheckBox enableSunChechBox = (CheckBox) findViewById(R.id.checkbox_enable_sun);
         enableSunChechBox.setChecked(getCliquePreferences().getSunEnabled());
 
-        setIdEditText.setHint(Long.toString(getCliquePreferences().getAccountId()));
-
-        setIdButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(getApplicationContext(), "don't be silly", Toast.LENGTH_SHORT).show();   //TODO: this should disappear
-            }
-        });
-
         setNameButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                ConnectivityManager connMgr = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
-                NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
-                if (networkInfo != null && networkInfo.isConnected()) {
-                    String name = setNameEditText.getText().toString();
-                    Log.i(TAG, "updating local self profile");
-                    try {
-                        DbSelfProfile.set(new Profile(name));
-                    } catch (CliqueDbException e) {
-                        e.printStackTrace();
-                        Log.i(TAG, "could not update local profile");
-                    }
-                    Log.i(TAG, "updating remote self profile");
-                    CliqueSyncer.getInstance(CliqueActivity_Settings.this).poke();  // TODO: get rid of that context shit
-                } else {
-                    Toast.makeText(getApplicationContext(), "no network", Toast.LENGTH_SHORT).show();
+                MyAssortedTools.hide_keyboard(CliqueActivity_Settings.this);
+                String name = setNameEditText.getText().toString();
+                Log.i(TAG, "updating local self profile");
+                try {
+                    DbSelfProfile.set(new Profile(name));
+                } catch (CliqueDbException e) {
+                    e.printStackTrace();
                 }
+                // i see no need to sync remotely here:
+//                ConnectivityManager connMgr = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+//                NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+//                if (networkInfo != null && networkInfo.isConnected()) {
+//                    String name = setNameEditText.getText().toString();
+//                    Log.i(TAG, "updating local self profile");
+//                    try {
+//                        DbSelfProfile.set(new Profile(name));
+//                    } catch (CliqueDbException e) {
+//                        e.printStackTrace();
+//                        Log.i(TAG, "could not update local profile");
+//                    }
+//                     Log.i(TAG, "updating remote self profile by syncing");
+//                     CliqueSyncer.getInstance(CliqueActivity_Settings.this).poke();  // TODO: get rid of that context shit? or not? figure it out
+//                } else {
+//                    Toast.makeText(getApplicationContext(), "no network", Toast.LENGTH_SHORT).show();
+//                }
             }
         });
 
@@ -100,7 +94,7 @@ public class CliqueActivity_Settings extends CliqueActivity implements AdapterVi
         });
 
         try {
-            setNameEditText.setHint(DbSelfProfile.get().getName());
+            setNameEditText.setText(DbSelfProfile.get().getName());
         } catch (CliqueDbException e) {
             e.printStackTrace();
         }
